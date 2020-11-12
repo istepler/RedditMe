92004b7b6f0920c9d50aa8907a47d8c383b71b6a
Andrey <andrey.krit@gmail.com>
2020-11-12 22:28:47 +0200

    initial commit

---
+++ RedditMe/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  RedditMe
+//
+//  Created by Andrey Krit on 12.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
