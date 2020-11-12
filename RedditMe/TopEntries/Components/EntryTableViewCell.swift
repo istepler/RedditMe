1a74c9ab1015447190295eacc3aa526a133120a0
Andrey <andrey.krit@gmail.com>
2020-11-12 23:42:54 +0200

    Added EntriesViewController UI

---
+++ RedditMe/TopEntries/Components/EntryTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EntryTableViewCell.swift
+//  RedditMe
+//
+//  Created by Andrey Krit on 12.11.2020.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var commentsLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+}
---
+++ RedditMe/TopEntries/TopEntriesViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  RedditMe
+//
+//  Created by Andrey Krit on 12.11.2020.
+//
+
+import UIKit
+
+class TopEntriesViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension TopEntriesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EntryTableViewCell", for: indexPath) as! EntryTableViewCell
+        return cell
+    }
+    
+}
+
